e5a09c5cb40e012e9e5b5a02c022ded8d05489be
crippa1337 <cristopher.torgrip@elev.ga.ntig.se>
2022-11-07 18:37:12 +0100

    finished DRY compliance

--- src/main.rs
+++ src/main.rs
@@ -3,57 +3,102 @@ use std::io::stdin;
 //TODO implement score tracking
 //TODO implement blazingly fast time tracking
 
-struct Hands {
-    rock: &str,
-    paper: &str,
-    scissors: &str,
+// for equality checks between instances
+#[derive(PartialEq)]
+enum Hands {
+    Rock,
+    Paper,
+    Scissors,
 }
 
-impl 
+// creates two methods using Hands
+impl Hands {
+    // win method, returns the winning matchup of a hand
+    fn win(&self) -> Hands {
+        match &self {
+            Hands::Rock => Hands::Scissors,
+            Hands::Paper => Hands::Rock,
+            Hands::Scissors => Hands::Paper,
+        }
+    }
 
-const DOTTED_LINE: &str = "----------------------------";
-fn main() {
-
-    println!("Welcome to 🚀RPS🚀\n");
-    loop {
-        println!("What do you want to play?\n{DOTTED_LINE}");
-        let mut input: String = String::new();
-        stdin()
-            .read_line(&mut input)
-            .expect("Failed to read input.");
-
-        let input: String = String::from(input.to_uppercase().trim());
-        let comp_pick: String = computer_hand();
-        println!(
-            "\nConfirmed input as: [{}🚀]\nComputer has chosen: [{comp_pick}🤖]\n\nOutcome: {}\n{DOTTED_LINE}",
-            input,
-            will_it_win(&input, &comp_pick)
-        );
+    // to_string method, returns an equal String to a hand
+    fn to_string(&self) -> String {
+        match &self {
+            Hands::Rock => "ROCK".to_string(),
+            Hands::Paper => "PAPER".to_string(),
+            Hands::Scissors => "SCISSORS".to_string(),
+        }
     }
 }
 
-fn computer_hand() -> String {
+// function to randomly make a hand for the computer, returns a Hands instance
+fn computer_hand() -> Hands {
+    // create comp_hand variable that is set to a random number from 1 to 3
     let comp_hand: u8 = rand::thread_rng().gen_range(1..=3);
+    // match the number to an instance of Hands
     match comp_hand {
-        1 => String::from("ROCK"),
-        2 => String::from("PAPER"),
-        3 => String::from("SCISSORS"),
+        1 => Hands::Rock,
+        2 => Hands::Paper,
+        3 => Hands::Scissors,
+        // out of reach
         _ => unreachable!(),
     }
 }
 
+const DOTTED_LINE: &str = "------------------------------------------------";
 const WIN: &str = "You win! 🚀🤑🚀";
 const EVEN: &str = "It's even! 😐🤨😴";
 const LOSS: &str = "You lose! 💀😭🤬";
+fn main() {
+    println!(
+        "{}\nWelcome to 🚀RPS🚀\nInput 'QUIT' to quit 😁",
+        DOTTED_LINE
+    );
+    'main_loop: loop {
+        let comp_pick = computer_hand();
+        let player_pick = loop {
+            println!("Input your hand: ROCK🪨, PAPER📃 or SCISSORS✂️\n{DOTTED_LINE}");
+            let mut input: String = String::new();
+            stdin()
+                .read_line(&mut input)
+                .expect("Failed to read input.");
 
-fn will_it_win(hand: &String, opp_hand: &String) -> String {
-    // Check struct for hand key
-    // if opp_hand is the same as hand key value return win
-    // if opp_hand = hand return tie
-    // else return loose
-    let struct_value = match hand {
-        "ROCK" => ,
-        "PAPER" =>,
-        "SCISSORS" =>,
+            let input = String::from(input.to_uppercase().trim());
+            break match input.as_str() {
+                "ROCK" => Hands::Rock,
+                "PAPER" => Hands::Paper,
+                "SCISSORS" => Hands::Scissors,
+                "QUIT" => break 'main_loop,
+                _ => {
+                    println!(
+                        "{} is an invalid input, please try again.\n{}",
+                        input, DOTTED_LINE
+                    );
+                    continue;
+                }
+            };
+        };
+        // result variable to be printed at game conclusion
+        let result: String;
+        // using PartialEq, see if player_pick (Hands) is equal to comp_pick (Hands)
+        if player_pick == comp_pick {
+            // set result to even since both inputs are the same
+            result = String::from(EVEN)
+        // see if the winning matchup of player_pick is equal to comp_pick
+        } else if player_pick.win() == comp_pick {
+            // set result to win since the winning matchup is achieved
+            result = String::from(WIN)
+        // only case left is if the player isn't even or won against the computer, which means it's a loss
+        } else {
+            // set result to loss
+            result = String::from(LOSS)
+        };
+
+        println!(
+            "Confirmed pick as: [{}]\nSuperA.I picks:    [{}]\n{result}\n{DOTTED_LINE}",
+            player_pick.to_string(),
+            comp_pick.to_string(),
+        );
     }
 }
