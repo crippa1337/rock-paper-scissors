5aa47af3b5770949ffacb6400953728c71c8bfa0
Cristopher T <cristopher.torgrip@elev.ga.ntig.se>
2022-11-07 13:59:57 +0100

    DRY compliance W.I.P

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,16 @@ use std::io::stdin;
 //TODO implement score tracking
 //TODO implement blazingly fast time tracking
 
+struct Hands {
+    rock: &str,
+    paper: &str,
+    scissors: &str,
+}
+
+impl 
+
+const DOTTED_LINE: &str = "----------------------------";
 fn main() {
-    const DOTTED_LINE: &str = "----------------------------";
 
     println!("Welcome to 🚀RPS🚀\n");
     loop {
@@ -17,9 +25,9 @@ fn main() {
         let input: String = String::from(input.to_uppercase().trim());
         let comp_pick: String = computer_hand();
         println!(
-            "\nConfirmed input as: [{}]\nComputer has chosen: [{comp_pick}]\n\nOutcome: {}\n{DOTTED_LINE}",
+            "\nConfirmed input as: [{}🚀]\nComputer has chosen: [{comp_pick}🤖]\n\nOutcome: {}\n{DOTTED_LINE}",
             input,
-            will_it_win(input.clone(), comp_pick.clone())
+            will_it_win(&input, &comp_pick)
         );
     }
 }
@@ -34,33 +42,18 @@ fn computer_hand() -> String {
     }
 }
 
-fn will_it_win(hand: String, opp_hand: String) -> String {
-    const WIN: &str = "You win! 🤑";
-    const EVEN: &str = "It's even! 😐";
-    const LOSS: &str = "You lose! 💀";
+const WIN: &str = "You win! 🚀🤑🚀";
+const EVEN: &str = "It's even! 😐🤨😴";
+const LOSS: &str = "You lose! 💀😭🤬";
 
-    let win_or_loss: String = match hand.as_str() {
-        "ROCK" => match opp_hand.as_str() {
-            "ROCK" => String::from(EVEN),
-            "PAPER" => String::from(LOSS),
-            "SCISSORS" => String::from(WIN),
-            _ => unimplemented!(),
-        },
-
-        "PAPER" => match opp_hand.as_str() {
-            "ROCK" => String::from(WIN),
-            "PAPER" => String::from(EVEN),
-            "SCISSORS" => String::from(LOSS),
-            _ => unimplemented!(),
-        },
-
-        "SCISSORS" => match opp_hand.as_str() {
-            "ROCK" => String::from(LOSS),
-            "PAPER" => String::from(WIN),
-            "SCISSORS" => String::from(EVEN),
-            _ => unimplemented!(),
-        },
-        _ => unimplemented!(),
-    };
-    return win_or_loss;
+fn will_it_win(hand: &String, opp_hand: &String) -> String {
+    // Check struct for hand key
+    // if opp_hand is the same as hand key value return win
+    // if opp_hand = hand return tie
+    // else return loose
+    let struct_value = match hand {
+        "ROCK" => ,
+        "PAPER" =>,
+        "SCISSORS" =>,
+    }
 }
