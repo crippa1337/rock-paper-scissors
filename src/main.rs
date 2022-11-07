d5cd695b819acc1488e95c94598a164ab307f639
crippa1337 <cristopher.torgrip@elev.ga.ntig.se>
2022-11-07 22:34:58 +0100

    computer_hand() with less code and added rockets

--- src/main.rs
+++ src/main.rs
@@ -35,10 +35,8 @@ impl Hands {
 
 // function to randomly make a hand for the computer, returns a Hands instance
 fn computer_hand() -> Hands {
-    // create comp_hand variable that is set to a random integer from 1 to 3
-    let comp_hand: u8 = rand::thread_rng().gen_range(1..=3);
-    // match the number to an instance of Hands
-    match comp_hand {
+    // matches a random integer from 1 to 3 and returns a random Hands instance
+    match rand::thread_rng().gen_range(1..=3) {
         1 => Hands::Rock,
         2 => Hands::Paper,
         3 => Hands::Scissors,
@@ -53,7 +51,7 @@ const EVEN: &str = "It's even! 😐🤨😴";
 const LOSS: &str = "You lose! 💀😭🤬";
 fn main() {
     println!(
-        "{}\nWelcome to Rock, Paper, Scissors\n🚀 Written in 100% Rust 🚀\nInput 'QUIT' to quit 😁",
+        "{}\nWelcome to the 🚀blazingly fast🚀 Rock, Paper, Scissors\nInput 'QUIT' to quit 😁",
         DOTTED_LINE
     );
 
@@ -86,7 +84,7 @@ fn main() {
         let instant = Instant::now();
         // assign a random hand to comp_pick
         let comp_pick = computer_hand();
-        // result variable to be printed at game conclusion
+
         let result: String;
         // using PartialEq, see if player_pick (Hands) is equal to comp_pick (Hands)
         if player_pick == comp_pick {
@@ -96,14 +94,14 @@ fn main() {
         } else if player_pick.win() == comp_pick {
             // set result to win since the winning matchup is achieved
             result = String::from(WIN)
-        // only case left is if the player hasn't won or is even against the computer, which means it's a loss
+        // only case left is if the player hasn't won or isn't even against the computer, which means it's a loss
         } else {
             // set result to loss
             result = String::from(LOSS)
         };
 
         println!(
-            "[TIME: {:?}]\nConfirmed pick as: [{}]\nSuperA.I picks:    [{}]\n{result}\n{DOTTED_LINE}",
+            "🚀 [TIME: {:?}] 🚀\nConfirmed pick as: [{}]\nSuperA.I picks:    [{}]\n{result}\n{DOTTED_LINE}",
             // how many seconds has elapsed since the instant Instant was created
             instant.elapsed(),
             player_pick.to_string(),
